fac874204de76ad17c5161754f71f1471787a2ae
MATT | ADL | BLOCK #0080 <mattyoungaviation@gmail.com>
2026-07-26 19:46:08 -0700

    Expose ground trail rendering methods

--- src/game/v3/render.js
+++ src/game/v3/render.js
@@ -1,9 +1,11 @@
 import { renderSceneMethods } from './renderScene.js';
 import { renderEnemyMethods } from './renderEnemies.js';
 import { renderPlayerMethods } from './renderPlayer.js';
+import { renderGoldTrailMethods } from './renderGoldTrail.js';
 
 export const renderMethods = {
   ...renderSceneMethods,
   ...renderEnemyMethods,
-  ...renderPlayerMethods
+  ...renderPlayerMethods,
+  ...renderGoldTrailMethods
 };
